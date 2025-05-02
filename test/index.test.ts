9b8801f7b959ac1b841e02799f1f6bf1d9cdb409
Wojstan <51679670+Wojstan@users.noreply.github.com>
2025-05-02 22:18:58 +0200

    Add scripts for linting (#2)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
+
 describe('index', () => {
   it('should run test', () => {
-    expect(true).toBe(true);
-  });
-});
+    expect(true).toBe(true)
+  })
+})
